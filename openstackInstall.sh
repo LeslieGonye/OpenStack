36005e6b55f8179d110da6dc427190e192e33a74
LeslieGonye <59709309+LeslieGonye@users.noreply.github.com>
2024-03-05 10:53:33 +0200

    Update openstackInstall.sh

--- openstackInstall.sh
+++ openstackInstall.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-sudo useradd -s /bin/bash -d /opt/stack -m stack
-sudo chmod -R 750 /opt/stack
-echo "### STEP 1 DONE!" 
+sudo useradd -s /bin/bash -d /opt/stack -m stack;
+sudo chmod -R 750 /opt/stack;
+echo "### STEP 1 DONE!"; 
 
-echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack 
-sudo -u stack -i
-echo "### STEP 2 DONE!" 
+echo "stack ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/stack; 
+sudo -u stack -i;
+echo "### STEP 2 DONE!"; 
 
-git clone https://opendev.org/openstack/devstack 
-cd devstack 
+git clone https://opendev.org/openstack/devstack; 
+cd devstack; 
 
-wget https://github.com/LeslieGonye/OpenStack/blob/main/local.conf
-echo "### STEP 3 DONE!" 
+wget https://github.com/LeslieGonye/OpenStack/blob/main/local.conf;
+echo "### STEP 3 DONE!"; 
 
-./devstack/stack.sh 
+./devstack/stack.sh;
 
 echo "IMPORTANT - Change Password for 
 ADMIN_PASSWORD 
